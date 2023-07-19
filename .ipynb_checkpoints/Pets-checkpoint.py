f7e2a46743d783c2bb27a53602becae90e9f278d
Casey5316 <caseymiller.0341@gmail.com>
2023-07-19 12:10:46 -0400

    Created the Pets class for Problem 1 of Ch10Ex

---
+++ .ipynb_checkpoints/Pets-checkpoint.py
@@ -0,0 +1,78 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "6df69e67",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "class Pets:\n",
+    "    def __init__(self, name, animal_type, age):\n",
+    "        self.__name = name\n",
+    "        self.__animal_type = animal_type\n",
+    "        self.__age = age\n",
+    "        \n",
+    "        \n",
+    "    \n",
+    "    \n",
+    "    def set_name(self, name):\n",
+    "        self.__name = name\n",
+    "        \n",
+    "        \n",
+    "    \n",
+    "    \n",
+    "    def set_type(self, animal_type):\n",
+    "        self.__animal_type = animal_type\n",
+    "        \n",
+    "        \n",
+    "        \n",
+    "        \n",
+    "    def set_age(self, age):\n",
+    "        self.__age = age\n",
+    "        \n",
+    "        \n",
+    "        \n",
+    "        \n",
+    "    def get_name(self):\n",
+    "        return self.__name\n",
+    "    \n",
+    "    \n",
+    "    \n",
+    "    \n",
+    "    def get_type(self):\n",
+    "        return self.__animal_type\n",
+    "    \n",
+    "    \n",
+    "    \n",
+    "    \n",
+    "    def get_age(self):\n",
+    "        return self.__age\n",
+    "    \n",
+    "    \n",
+    "    "
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.9"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
