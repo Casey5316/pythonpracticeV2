a0b7b2aa0a9363ba5ef8c2a8bb5efeb30b88fef9
Casey5316 <caseymiller.0341@gmail.com>
2023-07-31 21:37:12 -0400

    Completed Ch11Ex with SnagIt iteration

--- employees.py
+++ employees.py
@@ -1,94 +1,109 @@
 class Employee:
     def __init__(self, name, number):
-        self.__employee_name = name
-        self.__employee_num = number 
+        self.__name = name
+        self.__number = number
+        
         
         
         
     def set_name(self, name):
-        self.__employee_name = name
+        self.__name = name
+        
         
         
         
     def set_number(self, number):
-        self.__employee_num = number
+        self.__number = number 
+        
         
         
         
     def get_name(self):
-        return self.__employee_name
+        return self.__name
+    
     
     
     
     def get_number(self):
-        return self.__employee_num
+        return self.__number
+    
     
     
     
     def __str__(self):
-        return f'Employee name: {self.__employee_name}\nEmployee number: {self.__employee_num}'
+        return f'Employee name: {self.__name}\nEmployee number: {self.__number}'
     
     
     
+    
+    
+    
+
     
 class ProductionWorker(Employee):
-    def __init__(self, name, number, shift_no, wage):
+    def __init__(self, name, number, shift, pay):
         Employee.__init__(self, name, number)
         
-        self.__shift_no = shift_no
-        self.__wage = wage
+        self.__shift = shift
+        self.__pay = pay
         
         
         
         
-    def set_shift_no(self, shift_no):
-        self.__shift_no = shift_no
+    def set_shift(self, shift):
+        self.__shift = shift
         
         
         
         
-    def set_wage(self, wage):
-        self.__wage = wage
+    def set_pay(self, pay):
+        self.__pay = pay
         
         
         
         
-    def get_shift_no(self):
-        return self.__shift_no
+    def get_shift(self):
+        return self.__shift
     
     
     
     
-    def get_wage(self):
-        return self.__wage
+    def get_pay(self):
+        return self.__pay
+    
+    
+    
+    
+    
     
     
     
     
 class ShiftSupervisor(Employee):
-    def __init__(self, name, number, salary, prod_bonus):
+    def __init__(self, name, number, annual_salary, bonus):
         Employee.__init__(self, name, number)
         
-        self.__salary = salary 
-        self.__bonus = prod_bonus
-        
-        
-        
-        
-    def set_salary(self, salary):
-        self.__salary = salary
-        
-        
-        
-        
-    def set_bonus(self, prod_bonus):
+        self.__annual_salary = annual_salary
         self.__bonus = bonus
         
         
         
         
+    def set_salary(self, annual_salary):
+        self.__annual_salary = annual_salary
+        
+        
+        
+        
+    def set_bonus(self, bonus):
+        self.bonus = bonus
+        
+        
+        
+        
     def get_salary(self):
-        return self.__salary
+        return self.__annual_salary
+    
     
     
     
